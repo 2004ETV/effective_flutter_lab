6075d1b8c459f2e0e30d905f41e036d7f7aa2d85
Gennadii <ndynagn@gmail.com>
2023-12-07 22:29:42 +0600

    [Feat] Add models for tariffs and services

---
+++ lib/src/screens/profile/domain/models/tariffs_model.dart
@@ -0,0 +1,11 @@
+class TariffsModel {
+  const TariffsModel({
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+  });
+
+  final String icon;
+  final String title;
+  final String? subtitle;
+}
