3aa2f1b967689ac2879a3d61504f6e82237d49ad
Gennadii <ndynagn@gmail.com>
2023-12-07 22:30:59 +0600

    [Feat] Add reuse widgets

---
+++ lib/src/screens/profile/presentation/widgets/tariffs_cell.dart
@@ -0,0 +1,24 @@
+import 'package:effective_flutter_lab/src/common/widgets/svg_icon.dart';
+import 'package:effective_flutter_lab/src/screens/profile/domain/models/tariffs_model.dart';
+import 'package:flutter/material.dart';
+
+class TariffsCell extends StatelessWidget {
+  const TariffsCell({
+    super.key,
+    required this.model,
+    required this.onTap,
+  });
+
+  final TariffsModel model;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onTap,
+      leading: SvgIcon(icon: model.icon),
+      title: Text(model.title),
+      subtitle: model.subtitle != null ? Text(model.subtitle!) : null,
+    );
+  }
+}
